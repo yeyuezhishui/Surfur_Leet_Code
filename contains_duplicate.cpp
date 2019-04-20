d34ff4117ad868c453260d8dbc1393049295a512
dmmlcloud <41765012+dmmlcloud@users.noreply.github.com>
2019-04-20 16:54:46 +0800

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<int,int> umap;
         for(int i = 0;i<nums.size();i++)
         {
-            if(umap.find(nums[i]) != umap)  return true;
+            if(umap.find(nums[i]) != umap.end())  return true;
             else    umap[nums[i]] = 1;
         }
         return false;
